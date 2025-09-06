60760812645bd51ec1b71512b0636311ff4c094a
Mosè <capelli.mose.studente@itispaleocapa.it>
2025-09-06 22:54:02 +0100

    Creazione di una griglia per il sudoku

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[,] grid = new int[9, 9];
+
+        // Inizializza la griglia a 0
+        for (int r = 0; r < 9; r++)
+        {
+            for (int c = 0; c < 9; c++)
+            {
+                grid[r, c] = 0;
+            }
+        }
+
+        // Stampa la griglia
+        int col=0;
+        int rig=0;
+        for (int r = 0; r < 9; r++)
+        {
+
+
+            if (rig == 3)
+            {
+                Console.WriteLine("---------------------");
+                rig = 0;
+            }
+            for (int c = 0; c < 9; c++)
+            {
+                Console.Write(grid[r, c] + " ");
+                col++;
+                if (col % 3 == 0 && col != 0 && col != 9)
+                {
+                    Console.Write("| ");
+                    
+                }
+                else if (col == 9)
+                {
+                    col = 0;
+                }
+            }
+            Console.WriteLine();
+            rig++;
+        }
+    }
+}
+
